32b4c0d697c3a3a46a18748beaff7e256a67adbd
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-09 12:24:29 +0530

    add last message

--- models/Conversation.js
+++ models/Conversation.js
@@ -5,6 +5,12 @@ const conversationSchema = new mongoose.Schema(
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     isGroup: { type: Boolean, default: false },
     name: { type: String },
+    lastMessage: {
+      text: String,
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      createdAt: Date,
+    },
+    updatedAt: { type: Date },
   },
   { timestamps: true }
 );
