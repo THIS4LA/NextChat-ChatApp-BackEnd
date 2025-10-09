36ea1aa4b047ea9778ed104d866eb2eaa78bfcda
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-09 12:26:24 +0530

    update send message func and add typing and stop typing functions

--- socket/socket.js
+++ socket/socket.js
@@ -1,23 +1,46 @@
 export default function socketHandler(io) {
-  io.on("connection", (socket,cb) => {
-    console.log("🔌 User connected:", socket.id);
-    cb("Hello from server");
+  io.on("connection", (socket) => {
+    console.log("✅ User connected:", socket.id);
 
     // Join a conversation room
-    socket.on("joinRoom", (conversationId) => {
+    socket.on("joinConversation", (conversationId) => {
       socket.join(conversationId);
-      cb(`User ${socket.id} joined room ${conversationId}`);
+      console.log(`User ${socket.id} joined conversation ${conversationId}`);
     });
 
-    // Handle message send
-    socket.on("sendMessage", (data) => {
-      // data: { conversationId, senderId, text }
-      io.to(data.conversationId).emit("newMessage", data);
+    //Typing event
+    socket.on("typing", ({ conversationId, user }) => {
+      // Broadcast to others in the same room
+      socket.to(conversationId).emit("userTyping", user);
     });
 
-    // Disconnect
+    //Stop typing event
+    socket.on("stopTyping", ({ conversationId, user }) => {
+      socket.to(conversationId).emit("userStoppedTyping", user);
+    });
+
+    // Handle sending a message
+    socket.on("sendMessage", (messageData) => {
+      const receivedMessageData = {
+        _id: Date.now(),
+        text: messageData.text,
+        createdAt: messageData.createdAt,
+        sender: "other",
+        otherUser: {
+          username: messageData.sender.userName,
+          avatar: messageData.sender.avatar,
+        },
+      };
+      socket
+        .to(messageData.conversationId)
+        .emit("newMessage", receivedMessageData);
+      console.log(
+        `New message in conversation ${messageData.conversationId} from ${messageData.sender.userName}`
+      );
+    });
+
     socket.on("disconnect", () => {
-      cb("❌ User disconnected:", socket.id);
+      console.log("❌ User disconnected:", socket.id);
     });
   });
 }
