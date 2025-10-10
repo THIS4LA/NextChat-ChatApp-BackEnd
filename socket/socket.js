9e14f8d05998855ee3e5d5e45b15c7ba5c0959fd
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-10-10 23:43:05 +0530

    add socket functions for show online status of users

--- socket/socket.js
+++ socket/socket.js
@@ -8,6 +8,25 @@ export default function socketHandler(io) {
       console.log(`User ${socket.id} joined conversation ${conversationId}`);
     });
 
+    // Store online users
+    socket.on("userConnected", (userId) => {
+      onlineUsers.set(userId, socket.id);
+      io.emit("updateOnlineUsers", Array.from(onlineUsers.keys())); // broadcast
+      console.log(`User ${userId} is online`);
+    });
+
+    // when user disconnects
+    socket.on("disconnect", () => {
+      for (let [userId, id] of onlineUsers.entries()) {
+        if (id === socket.id) {
+          onlineUsers.delete(userId);
+          io.emit("updateOnlineUsers", Array.from(onlineUsers.keys()));
+          console.log(`User ${userId} went offline`);
+          break;
+        }
+      }
+    });
+
     //Typing event
     socket.on("typing", ({ conversationId, user }) => {
       // Broadcast to others in the same room
