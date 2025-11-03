9f3f21473266a645ec9441b923c86ebcaebcbb78
Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2025-11-03 13:26:10 +0530

    add env frontend URL to CORS configuration

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://admin.socket.io"],
+    origin: ["http://localhost:3000", "https://admin.socket.io", process.env.FRONTEND_URL],
     credentials: true,
   },
 });
